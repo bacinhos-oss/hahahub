3b08b7e4e6d670602210987390f6b237cb8e8da0
Dejan <bacinhos@gmail.com>
2026-05-06 11:51:08 +0200

    fix: add BCC to admin on all emails

--- send-email.js
+++ send-email.js
@@ -198,6 +198,7 @@ module.exports = async function handler(req, res) {
     const result = await resend.emails.send({
       from: FROM,
       to,
+      bcc: 'info@hahahub.art',
       subject,
       html,
     });
