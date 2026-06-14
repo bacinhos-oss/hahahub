72c239d3a653544729b8d28af9a30d343147920a
Dejan <bacinhos@gmail.com>
2026-06-14 21:10:29 +0200

    Revert "feat: HHH glitch loading screen on startup"
    This reverts commit bd81328dfcf947178b7d5a85e23c37638d1ef63c.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import UploadPage from './pages/UploadPage'
 import CookieBanner from './components/CookieBanner'
 import FeedbackButton from './components/FeedbackButton'
 import OnboardingWizard from './components/OnboardingWizard'
-import LoadingScreen from './components/LoadingScreen'
 import NotFoundPage from './pages/NotFoundPage'
 import PricingPage from './pages/PricingPage'
 import FAQPage from './pages/FAQPage'
@@ -27,7 +26,6 @@ import DealsPipelinePage from './pages/DealsPipelinePage'
 const ADMIN_EMAIL = 'bacinhos@gmail.com'
 
 const App: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(true)
   const [currentPage, setCurrentPageRaw] = useState<Page>('landing')
   const setCurrentPage = (page: Page) => { window.scrollTo(0, 0); setCurrentPageRaw(page); }
   const [currentUser, setCurrentUser] = useState<User | null>(null)
@@ -458,7 +456,6 @@ const App: React.FC = () => {
 
   return (
     <div className="min-h-screen">
-      {isLoading && <LoadingScreen onDone={() => setIsLoading(false)} />}
       {renderPage()}
       <CookieBanner />
       <FeedbackButton />
